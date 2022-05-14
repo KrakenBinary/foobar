14b0dab970a80e933eb37514652c7f8ef56be540
KrakenBinary <krakenbinary@gmail.com>
2022-05-13 21:09:15 -0500

    volunteered

--- bomb-baby/solution.py
+++ bomb-baby/solution.py
@@ -5,10 +5,9 @@ Lets rock and roll!
 '''
 
 
-def solution(mach, facula):
+def old_solution(mach, facula):
     '''What are we up to now?'''
-    mach = int(mach)
-    facula = int(facula)
+    mach, facula = int(mach), int(facula)
     if mach - facula == 0:
         print('# R1 #')
         return 0
@@ -16,7 +15,10 @@ def solution(mach, facula):
 
 
 def bad_looper(mach, facula):
-    '''bad idea but it gets me started'''
+    '''
+    bad idea but it gets me started
+    cant run the big ones through this :( sad panda
+    '''
     count = 0
     while mach != facula:
         if mach == 1 and facula == 1:
@@ -32,3 +34,38 @@ def bad_looper(mach, facula):
             facula = facula - mach
     print('# R3 #')
     return count
+
+
+def solution(mach, facula):
+    '''
+    lets figure out how to run the big numbers now
+    while loop worked
+        learned that we have to use:
+            m-f = m (step.n-1)
+            f-m = f (step.n-1)
+    use integer division and remainders to determin
+    how many times to subtract before we flip to the other
+    method. This gives us a nice shortcut.
+    '''
+    try:
+        hcam, alucaf = int(mach), int(facula)
+    except Exception:
+        return 'impossible'
+    if hcam == alucaf or hcam <= 0 or alucaf <= 0:
+        return 'impossible'
+    true_count = 0
+    while hcam != alucaf:
+        if hcam > alucaf:
+            division = (hcam-alucaf)//alucaf
+            true_remainder = ((hcam-alucaf) % alucaf > 0)
+            subtractions_count = division + true_remainder
+            true_count += subtractions_count
+            hcam = hcam - subtractions_count * alucaf
+        elif alucaf > hcam:
+            division = (alucaf-hcam)//hcam
+            true_remainder = (alucaf-hcam) % hcam > 0
+            subtractions_count = division + true_remainder
+            true_count += subtractions_count
+            alucaf = alucaf - subtractions_count * hcam
+
+    return str(true_count) if (hcam, alucaf) == (1, 1) else 'impossible'
